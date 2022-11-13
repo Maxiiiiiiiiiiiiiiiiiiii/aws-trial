62d4c1d17a09a1ff7bc6ba0e7d3bc36b5281d481
Max <117511558+Maxiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2022-11-13 13:56:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http')
+
+var port = 1010
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('नाष्ट्या ला चालायचा का  डार्लिंग ?\n')
+})
+
+server.listen(port)
+
+console.log('Server running at http://localhost:' + port)
